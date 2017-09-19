41bb11db069930b79f0687e291434a67ca930294
denis <deoniisii@gmail.com>
2017-09-19 20:56:59 +0300

    Реализовал 1 тест и 2 тест,  реализовал сложение дробей

---
+++ src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/LAB33/Fraction/FractionUtils.java
@@ -0,0 +1,60 @@
+package ua.com.juja.core.ReferenceTypesofDataLines.BigNumbers.LAB33.Fraction;
+
+/**
+ * Created by Denis Oleynyk on 19.09.17.
+ * mail: deoniisii@gmail.com
+ */
+
+import java.math.BigInteger;
+
+public class FractionUtils {
+
+    public static BigInteger[] sum(BigInteger[] x, BigInteger[] y) {
+
+        BigInteger[] fractionSum = new BigInteger[2];
+        /*BODY*/
+        //Сложение дробей с одинаковыми знаменателями
+        if (x[1].equals(y[1])) {
+            fractionSum[0] = x[0].add(y[0]);// сложение числителей дроби
+            fractionSum[1] = y[1];//знаменатель
+        } else {
+            // TODO Сложение обыкновенных дробей
+
+          fractionSum =  additionOfFractions(x, y);
+            //сократить полученную дробь
+            //Чтобы сократить дробь   m n  нужно найти наибольший общий делитель ее числителя и знаменателя: НОД(m,n),
+            // после чего поделить числитель и знаменатель дроби на это число. Если НОД(m,n)=1, то дробь сократить нельзя.
+
+        }
+
+        return fractionSum;
+    }
+
+    //сложение дробей
+    private static BigInteger[] additionOfFractions(BigInteger[] x, BigInteger[] y) {
+
+        BigInteger []sum = new BigInteger[2];
+
+
+        BigInteger numeratorX = x[0];
+        BigInteger denominatorX = x[1];
+        BigInteger numeratorY = y[0];
+        BigInteger denominatorY = y[1];
+
+        sum[0] = (numeratorX.multiply(denominatorY)).add(numeratorY.multiply(denominatorX)) ;
+        sum[1] = denominatorX.multiply(denominatorY);
+
+
+        return sum;
+    }
+
+
+    private static BigInteger [] abbreviationOffraction(BigInteger[] x, BigInteger[] y){
+        BigInteger [] fractions = new BigInteger[2];
+
+        //Чтобы сократить дробь   m n  нужно найти наибольший общий делитель ее числителя и знаменателя: НОД(m,n),
+        // после чего поделить числитель и знаменатель дроби на это число. Если НОД(m,n)=1, то дробь сократить нельзя.
+
+        return fractions;
+    }
+}
---
+++ src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/LAB33/Fraction/Test.java
@@ -0,0 +1,32 @@
+package ua.com.juja.core.ReferenceTypesofDataLines.BigNumbers.LAB33.Fraction;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void Test2() {
+        BigInteger[] x = {new BigInteger("1"), new BigInteger("2")};
+        BigInteger[] y = {new BigInteger("1"), new BigInteger("2")};
+        BigInteger[] expected = {new BigInteger("1"), new BigInteger("1")};
+        BigInteger[] actual = FractionUtils.sum(x, y);
+
+        assertEquals(Arrays.toString(expected),Arrays.toString(actual));
+
+
+    }
+
+    @org.junit.Test
+    public void Test1() {
+        BigInteger[] x = {new BigInteger("1"), new BigInteger("2")};
+        BigInteger[] y = {new BigInteger("1"), new BigInteger("3")};
+        BigInteger[] expected = {new BigInteger("5"), new BigInteger("6")};
+        BigInteger[] actual = FractionUtils.sum(x, y);
+
+        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+        System.out.println(Arrays.toString(actual));
+
+    }
+}
