9dfe2f53a0b7ba97ea9d7fa729428f342c74b5d9
denis <deoniisii@gmail.com>
2017-09-20 15:46:46 +0300

    Реализовал 1 тест BigIntegerUtils.toBitStr(k)

--- src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/Lab34/toBitString/BigIntegerUtils.java
+++ src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/Lab34/toBitString/BigIntegerUtils.java
@@ -4,11 +4,12 @@ package ua.com.juja.core.ReferenceTypesofDataLines.BigNumbers.Lab34.toBitString;
  * Created by Denis Oleynyk on 20.09.17.
  * mail: deoniisii@gmail.com
  */
+
 import java.math.BigInteger;
 
 public class BigIntegerUtils {
     public static String toBitStr(BigInteger arg) {
-        /*BODY*/
-        return "";
+
+        return arg.toString(2);
     }
 }
--- src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/Lab34/toBitString/Test.java
+++ src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/Lab34/toBitString/Test.java
@@ -5,6 +5,17 @@ import java.math.BigInteger;
 import static org.junit.Assert.assertEquals;
 
 public class Test {
+
+    @org.junit.Test
+    public void Test2() {
+        BigInteger k = new BigInteger("2");
+        String expected = "10";
+        String actual = BigIntegerUtils.toBitStr(k);
+
+        assertEquals("BigIntegerUtils.toBitStr(\"2\")", expected, actual);
+
+    }
+
     @org.junit.Test
     public void Test1() {
         BigInteger k = new BigInteger("0");
