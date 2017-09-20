9be9d2135ed7c7168e9a27e0610a1389c427baab
denis <deoniisii@gmail.com>
2017-09-20 18:35:35 +0300

    Реализовал 1 тест

--- src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/Lab35/PolyUtils/PolyUtils.java
+++ src/ua/com/juja/core/ReferenceTypesofDataLines/BigNumbers/Lab35/PolyUtils/PolyUtils.java
@@ -4,11 +4,21 @@ package ua.com.juja.core.ReferenceTypesofDataLines.BigNumbers.Lab35.PolyUtils;
  * Created by Denis Oleynyk on 20.09.17.
  * mail: deoniisii@gmail.com
  */
+
 import java.math.BigInteger;
 
 public class PolyUtils {
     public static BigInteger eval(BigInteger[] poly, BigInteger arg) {
         /*BODY*/
-        return arg;
+        BigInteger result = new BigInteger("0");
+
+        for (int i = 0; i < poly.length; i++) {
+
+            int pow = poly.length - i - 1;
+            result = result.add(poly[i].multiply(arg.pow(pow)));
+
+        }
+
+        return result;
     }
 }
