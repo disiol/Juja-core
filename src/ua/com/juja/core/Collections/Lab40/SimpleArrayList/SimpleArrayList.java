65bc969ed7b1ace56f761bdd96174ee38b85c4bb
denis <deoniisii@gmail.com>
2017-10-28 15:12:06 +0300

    Сддал второй блок  тестов/

---
+++ src/ua/com/juja/core/Collections/Lab40/SimpleArrayList/SimpleArrayList.java
@@ -0,0 +1,90 @@
+package ua.com.juja.core.Collections.Lab40.SimpleArrayList;
+
+import java.util.Iterator;
+import java.util.ConcurrentModificationException;
+import java.util.NoSuchElementException;
+
+public class SimpleArrayList <E> implements SimpleList<E> {
+
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private E[] data;
+    private int size = 0;
+
+    public SimpleArrayList() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+
+    public SimpleArrayList(int initialCapacity) {
+        this.data = (E[]) new Object[initialCapacity];
+    }
+
+    @Override
+    public boolean add(E newElement) {
+        ensureCapacity(size + 1);
+        data[size] = newElement;
+        size++;
+        return true;
+    }
+
+    @Override
+    public E get(int index) {
+        rangeCheck(index);
+        return data[index];
+    }
+
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public E remove(int index) {
+        rangeCheck(index);
+        E oldValue = data[index];
+        int numMoved = size - index - 1;
+        System.arraycopy(data, index + 1, data, index, numMoved);
+        data[--size] = null;
+        return oldValue;
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || size < index) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > data.length) {
+            int newCapacity = Math.max(minCapacity, data.length + (data.length >> 1));
+            E[] newData = (E[]) new Object[newCapacity];
+            System.arraycopy(data, 0, newData, 0, data.length);
+            this.data = newData;
+        }
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+
+        //TODO
+        return null;
+    }
+    /*BODY*/
+
+}
+
+interface SimpleList<E> {
+    public boolean add(E newElement);
+    public E get(int index);
+    public Iterator<E> iterator();
+    public int size();
+    public boolean isEmpty();
+    public E remove(int index);
+} 
---
+++ src/ua/com/juja/core/Exceptions/Lab38/AccountManager/Lab39/Try_with_resourcesInJava6/TryWithResource.java
@@ -0,0 +1,4 @@
+package ua.com.juja.core.Exceptions.Lab38.AccountManager.Lab39.Try_with_resourcesInJava6;
+
+public class TryWithResource {
+}
